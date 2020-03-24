dde1effe023b5e4b9951bd6aa86a6c77b7a83db1
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-03-24 14:50:18 -0500

    Topic 3

--- Topic 3/scraper.py
+++ Topic 3/scraper.py
@@ -1,10 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = ''
+url = 'https://www.dmacc.edu/Pages/welcome.aspx'
 response = requests.get(url)
 html = requests.get(url)
 f = open("requestResult.txt", "w+")
 f.writelines(str(html))
 f.close()
-soup = bs.BeautifulSoup(open("requestResult.txt"), 'html.parser')
+soup = BeautifulSoup(open("requestResult.txt"), 'html.parser')
 print(soup.prettify())
