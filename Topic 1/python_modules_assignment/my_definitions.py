e30c1177fba0efe993e7e073e7192ed9220779f7
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-03-24 13:25:48 -0500

    First assignment

---
+++ Topic 1/python_modules_assignment/my_definitions.py
@@ -0,0 +1,18 @@
+def greeting():
+    print('Welcome to my module')
+
+
+def author_of_code():
+    print('Bill McCulley is the author of this code')
+
+
+def print_dict(d):
+    for key in d:
+        print("key:", key, "Value:", d[key])
+
+
+def print_set(s):
+    print(s)
+
+
+    if __name__ == '__main__':
